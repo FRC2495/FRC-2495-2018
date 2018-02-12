48bd49bb865e6788ea5e0995664a6eb90f97b8bc
Joshua <Joshua@10.235.1.246>
2018-02-12 17:26:28 -0500

    grasper movement

--- src/org/usfirst/frc/team2495/robot/Grasper.java
+++ src/org/usfirst/frc/team2495/robot/Grasper.java
@@ -2,6 +2,7 @@
  * 
  */
 package org.usfirst.frc.team2495.robot;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
@@ -14,10 +15,11 @@ public class Grasper {
 	/**
 	 * 
 	 */
+	static final double MAX_PCT_OUTPUT = 1.0;
+	
 	WPI_TalonSRX grasperLeft , grasperRight; 
 	
 	public Grasper( WPI_TalonSRX grasperLeft_in, WPI_TalonSRX grasperRight_in) {
-		// TODO Auto-generated constructor stub
 		
 		grasperLeft = grasperLeft_in;
 		grasperRight = grasperRight_in;
@@ -28,12 +30,16 @@ public class Grasper {
 		//this might me wrong =j
 		grasperLeft.setInverted(true);
 		grasperRight.setInverted(false);
+		
+		grasperRight.follow(grasperLeft);	
 	}
 
-	public void grasp() { 
+	public void grasp() {
+		grasperLeft.set(ControlMode.PercentOutput, MAX_PCT_OUTPUT);
 	}
 	
 	public void release() {
+		grasperLeft.set(ControlMode.PercentOutput, -MAX_PCT_OUTPUT);
 	}
 }
 
