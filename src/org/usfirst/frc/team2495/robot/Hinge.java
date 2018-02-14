3397f1a7294a8a5f88a30b514e4db518f89b6cca
Joshua <Joshua@10.235.1.96>
2018-02-14 18:00:19 -0500

    hinge raise and lower started

--- src/org/usfirst/frc/team2495/robot/Hinge.java
+++ src/org/usfirst/frc/team2495/robot/Hinge.java
@@ -1,12 +1,16 @@
 
 package org.usfirst.frc.team2495.robot;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-//import com.ctre.phoenix.motorcontrol.ControlMode;
-//import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class Hinge {
 
+	static final double DESIRED_OUT_PUT = .5;
+	static final int TICKS_PER_REVOLUTION = 4096;
+	static final double DESIRED_DESTINATION  = 4096 * .25;
+	
 	WPI_TalonSRX hinge; 
 	
 	public Hinge (WPI_TalonSRX hinge_in) {
@@ -16,5 +20,13 @@ public class Hinge {
 		hinge.setNeutralMode(NeutralMode.Brake);
 		
 	}
+	public void raise() {
+		hinge.set(ControlMode.PercentOutput, DESIRED_OUT_PUT);
+		
+	}
+	
+		public void lower() { 
+			hinge.set(ControlMode.PercentOutput, DESIRED_OUT_PUT);
+		}
 	
 }
