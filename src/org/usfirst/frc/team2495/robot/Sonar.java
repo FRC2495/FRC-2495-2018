1ce2b6a11d1837159bbd736677ac92c207b34596
Gabriel <na>
2018-02-16 23:25:19 -0500

    minor cleanup

--- src/org/usfirst/frc/team2495/robot/Sonar.java
+++ src/org/usfirst/frc/team2495/robot/Sonar.java
@@ -5,12 +5,14 @@ import edu.wpi.first.wpilibj.*;
 public class Sonar{
 	
     private final double IN_TO_CM_CONVERSION = 2.54;
+    
     private boolean use_units;    //Are we using units or just returning voltage?
     private double min_voltage;	  //Minimum voltage the ultrasonic sensor can return
     private double voltage_range; //The range of the voltages returned by the sensor (maximum - minimum)
     private double min_distance;  //Minimum distance the ultrasonic sensor can return in inches
     private double distance_range;//The range of the distances returned by this class in inches (maximum - minimum)
-    AnalogInput channel;
+    private AnalogInput channel;
+    
     //constructor
     public Sonar(int _channel) {
         channel = new AnalogInput(_channel);
@@ -21,10 +23,12 @@ public class Sonar{
 		min_distance = 10.0; //accurate anywhere over 10in - maxdistance 
 		distance_range = 360 - min_distance;
     }
+    
     //constructor
     public Sonar(int _channel, boolean _use_units, double _min_voltage,
             double _max_voltage, double _min_distance, double _max_distance) {
         channel = new AnalogInput(_channel);
+        
         //only use unit-specific variables if we're using units
         if (_use_units) {
             use_units = true;
@@ -34,54 +38,65 @@ public class Sonar{
             distance_range = _max_distance - _min_distance;
         }
     }
+    
     // Just get the voltage.
     double getVoltage() {
         return channel.getVoltage();
     }
-    /* GetRangeInInches
+    
+    /* getRangeInInches
      * Returns the range in inches
      * Returns -1.0 if units are not being used
      * Returns -2.0 if the voltage is below the minimum voltage
      */
-
     double getRangeInInches() {
         double range;
+        
         //if we're not using units, return -1, a range that will most likely never be returned
         if (!use_units) {
             return -1.0;
         }
+        
         range = channel.getVoltage();
+        
         if (range < min_voltage) {
             return -2.0;
         }
+        
         //first, normalize the voltage
         range = (range - min_voltage) / voltage_range;
         //next, denormalize to the unit range
         range = (range * distance_range) + min_distance;
+        
         return range;
     }
-    /* GetRangeInCM
+    
+    /* getRangeInCM
      * Returns the range in centimeters
      * Returns -1.0 if units are not being used
      * Returns -2.0 if the voltage is below the minimum voltage
      */
-
     double getRangeInCM() {
         double range;
+        
         //if we're not using units, return -1, a range that will most likely never be returned
         if (!use_units) {
             return -1.0;
         }
+        
         range = channel.getVoltage();
+        
         if (range < min_voltage) {
             return -2.0;
         }
+        
         //first, normalize the voltage
         range = (range - min_voltage) / voltage_range;
         //next, denormalize to the unit range
         range = (range * distance_range) + min_distance;
         //finally, convert to centimeters
         range *= IN_TO_CM_CONVERSION;
+        
         return range;
     }
 }
